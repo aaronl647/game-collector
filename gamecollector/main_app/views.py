eef0487e0ccf67bb7a61d8142339de2f767b49a4
aaronl647 <50430212+aaronl647@users.noreply.github.com>
2020-04-14 12:22:35 -0400

    changed - to = on line 15

--- gamecollector/main_app/views.py
+++ gamecollector/main_app/views.py
@@ -12,7 +12,7 @@ class GamesCreate(CreateView):
     fields = ['name', 'developer', 'publisher', 'genre', 'release_year']
 
     def form_valid(self, form):
-        form.instance.user - self.request.user
+        form.instance.user = self.request.user
         return super().form_valid(form)
 
     
